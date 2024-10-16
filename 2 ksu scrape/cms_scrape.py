4a2f6643bced44299167dea73a507b298aa3a667
Shark1346 <agaldam3@students.kennesaw.edu>
2024-10-15 23:28:55 -0400

    Revert "10/15"
    This reverts commit 277996cfdde61bf98b319b6c920abc43e7ed9e0f.

--- 2 ksu scrape/cms_scrape.py
+++ 2 ksu scrape/cms_scrape.py
@@ -1,6 +1,6 @@
 '''
 This code goes out to the Web and gets a web page and parses it into local data.
-T0 use this code, you will first need to install the three packages being imported below using pip.
+To use this code, you will first need to install the three packages being imported below using pip.
 '''
 from bs4 import BeautifulSoup
 import requests
